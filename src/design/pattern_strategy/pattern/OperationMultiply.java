6caa6d9a325a26d5342266e54e01c1ba94248f9b
boy20046413 <boy6413036@gmail.com>
2015-04-15 15:35:36 +0800

    DesignPattern_StrategyPattern

---
+++ src/design/pattern_strategy/pattern/OperationMultiply.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package design.pattern_strategy.pattern;
+
+/**
+ *
+ * @author User
+ */
+public class OperationMultiply implements Strategy{
+   @Override
+   public int doOperation(int num1, int num2) {
+      return num1 * num2;
+   }
+}
---
+++ src/design/pattern_strategy/pattern/Strategy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package design.pattern_strategy.pattern;
+
+/**
+ *
+ * @author User
+ */
+public interface Strategy {
+   public int doOperation(int num1, int num2);
+}
